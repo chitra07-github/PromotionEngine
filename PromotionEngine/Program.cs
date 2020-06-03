d1d68a26c7470ea6c75578338813c49db815e7cc
Chitra Devi <Chitra.Devi@brandmuscle.com>
2020-06-03 11:54:19 +0530

    SKU class added

--- PromotionEngine/Program.cs
+++ PromotionEngine/Program.cs
@@ -12,4 +12,31 @@ namespace PromotionEngine
         {
         }
     }
+    public class SKU
+    {
+        public char SKUId { get; set; }
+        public decimal SKUPrice { get; set; }
+        public SKU(char SKUId)
+        {
+            this.SKUId = SKUId;
+            switch (SKUId)
+            {
+                case 'A':
+                    this.SKUPrice = 50;
+
+                    break;
+                case 'B':
+                    this.SKUPrice = 30;
+                    break;
+                case 'C':
+                    this.SKUPrice = 20;
+
+                    break;
+                case 'D':
+                    this.SKUPrice = 15;
+                    break;
+            }
+        }
+
+    }
 }
